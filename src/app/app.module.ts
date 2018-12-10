fee600fdd41c93bd03ec15fcf456eefdbfb42a2b
rafaelfcdk <rafaelfcdk@gmail.com>
2018-12-10 17:20:21 +0100

    First Commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,36 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
-
-import { AppComponent } from './app.component';
-
+import { BrowserModule } from "@angular/platform-browser";
+import { NgModule } from "@angular/core";
+import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
+import {
+  MatGridListModule,
+  MatButtonModule,
+  MatToolbarModule
+} from "@angular/material";
+import { Routes, RouterModule } from "@angular/router";
+import { AppComponent } from "./app.component";
+import { GameComponent } from "./game/game.component";
+import { FlexLayoutModule } from "@angular/flex-layout";
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [AppComponent, GameComponent],
   imports: [
-    BrowserModule
+    FlexLayoutModule,
+    BrowserAnimationsModule,
+    MatButtonModule,
+    MatToolbarModule,
+    MatGridListModule,
+    BrowserModule,
+    RouterModule.forRoot([
+      {
+        path: "gameoflife/:id",
+        component: GameComponent
+      },
+      {
+        path: "**",
+        component: GameComponent
+      }
+    ])
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
